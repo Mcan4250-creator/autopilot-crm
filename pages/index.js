346f25b3c6eb2056ea4b1f0bfb3afb00dfa26c6a
Mcan4250-creator <mevlutcan.akbulut@gmail.com>
2025-07-19 17:59:42 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Willkommen bei Autopilot CRM</h1>;
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <img src="/logo.png" alt="Akbulut Digital Logo" width="150" />
+      <h1>Willkommen bei Autopilot CRM</h1>
+    </div>
+  );
 }
