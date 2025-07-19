d94839ce2f3e22281a8ca1ec8cac698215afe7c8
Mcan4250-creator <mevlutcan.akbulut@gmail.com>
2025-07-19 18:10:23 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div style={{
@@ -9,8 +11,14 @@ export default function Home() {
       backgroundColor: "#f5f5f5",
       fontFamily: "Arial, sans-serif"
     }}>
+      <nav style={{ marginBottom: "40px" }}>
+        <Link href="/" style={{ margin: "0 20px", textDecoration: "none", color: "#0070f3" }}>Start</Link>
+        <Link href="/termine" style={{ margin: "0 20px", textDecoration: "none", color: "#0070f3" }}>Termine</Link>
+        <Link href="/einstellungen" style={{ margin: "0 20px", textDecoration: "none", color: "#0070f3" }}>Einstellungen</Link>
+      </nav>
+
       <img
-        src="/logo.png"
+        src="/Logo.png"
         alt="Akbulut Digital Logo"
         width="150"
         style={{ marginBottom: "20px" }}
